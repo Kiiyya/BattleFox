4a5926255a3a4f4e00d72ba27d911b3a7b8f7e25
Kiiya <snoewflaek@gmail.com>
2021-03-08 13:46:09 +0100

    add clap to rcon-cli

--- battlefield_rcon/src/bin/rcon-cli.rs
+++ battlefield_rcon/src/bin/rcon-cli.rs
@@ -1,10 +1,11 @@
-use std::io::{BufRead, Write, stdout};
+use std::{io::{BufRead, Write, stdout}, process::exit};
 
 #[macro_use]
 extern crate crossterm;
 
 use ascii::IntoAsciiString;
 use battlefield_rcon::rcon::{RconClient, RconError, RconQueryable, RconResult};
+use clap::{Arg, SubCommand};
 use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor};
 use dotenv::{dotenv, var};
 
@@ -20,86 +21,111 @@ async fn main() -> RconResult<()> {
         .unwrap();
     let password = var("BFOX_RCON_PASSWORD").unwrap_or("smurf".into());
 
-    println!("Connecting to {}:{} with password ***...", ip, port);
-    let rcon = RconClient::connect((ip.as_str(), port, password.as_str())).await?;
+    let matches = clap::App::new("rcon-cli")
+        .version("0.1")
+        .about("Extremely simple and BF4-specifics-unaware library to send and receive strings.")
+        .author("Kiiya (snoewflaek@gmail.com)")
+        .subcommand(SubCommand::with_name("query")
+            .about("Send single query and print result, instead of going into interactive mode")
+            .arg(Arg::with_name("query-words").min_values(1))
+        )
+        .get_matches();
+
+    // println!("Connecting to {}:{} with password ***...", ip, port);
+    let rcon = match RconClient::connect((ip.as_str(), port, password.as_str())).await {
+        Ok(rcon) => rcon,
+        Err(err) => {
+            println!("Failed to connect to Rcon at {}:{} with password ***: {:?}", ip, port, err);
+            exit(-1);
+        }
+    };
     // let bf4 = Bf4Client::new(rcon).await.unwrap();
-    println!("Connected!");
+    // println!("Connected!");
 
-    print!("-> ");
-    std::io::stdout().flush()?;
-    let stdin = std::io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line?;
-        let words = line.split(" ");
-        let mut words_ascii = Vec::new();
-        for word in words {
-            words_ascii.push(word.into_ascii_string()?);
-        }
-
-        let result = rcon.query(&words_ascii,
-            |ok| Ok(ok.to_owned()),
-            |err| Some(RconError::other(err)),
-        ).await;
-
-        match result {
-            Ok(ok) => {
-                let mut str = String::new();
-                for word in ok {
-                    str.push(' ');
-                    str.push_str(word.as_str());
-                }
-                execute!(
-                    stdout(),
-                    SetForegroundColor(Color::Black),
-                    SetBackgroundColor(Color::Green),
-                    Print("<- OK".to_string()),
-                    SetForegroundColor(Color::Green),
-                    SetBackgroundColor(Color::Reset),
-                    Print(str),
-                    ResetColor,
-                    Print("\n".to_string())
-                ).unwrap();
-            }
-            Err(err) => {
-                execute!(
-                    stdout(),
-                    SetForegroundColor(Color::Black),
-                    SetBackgroundColor(Color::Red),
-                ).unwrap();
-
-                match err {
-                    RconError::Other(str) => {
-                        // println!("{}", str.on_dark_red());
-                        execute!(
-                            stdout(),
-                            Print("<- Error".to_string()),
-                            SetForegroundColor(Color::Red),
-                            SetBackgroundColor(Color::Reset),
-                            Print(" ".to_string()),
-                            Print(str)
-                        ).unwrap();
-                    },
-                    RconError::ConnectionClosed => {
-                        print_error_type("Connection Closed").unwrap();
-                    },
-                    RconError::InvalidArguments {our_query: _} => {
-                        print_error_type("Invalid Arguments").unwrap();
-                    },
-                    RconError::UnknownCommand {our_query: _} => {
-                        print_error_type("Unknown Command").unwrap();
-                    },
-                    _ => panic!("Unexpected error: {:?}", err),
-                };
-                execute!(
-                    stdout(),
-                    ResetColor,
-                    Print("\n".to_string())
-                ).unwrap();
-            }
-        }
-
+    // if user provided "query" subcommand, just do that. Otherwise, go into interactive mode.
+    if let Some(singlequery) = matches.subcommand_matches("query") {
+        let words = singlequery.values_of("query-words").unwrap().collect::<Vec<_>>();
+        handle_input_line(words, &rcon).await?;
+    } else {
         print!("-> ");
         std::io::stdout().flush()?;
+        let stdin = std::io::stdin();
+        for line in stdin.lock().lines() {
+            let line = line?;
+            let words = line.split(" ");
+            handle_input_line(words, &rcon).await?;
+            print!("-> ");
+            std::io::stdout().flush()?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn handle_input_line(words: impl IntoIterator<Item = &str>, rcon: &RconClient) -> RconResult<()> {
+    let mut words_ascii = Vec::new();
+    for word in words {
+        words_ascii.push(word.into_ascii_string()?);
+    }
+    let result = rcon.query(&words_ascii,
+        |ok| Ok(ok.to_owned()),
+        |err| Some(RconError::other(err)),
+    ).await;
+    match result {
+        Ok(ok) => {
+            let mut str = String::new();
+            for word in ok {
+                str.push(' ');
+                str.push_str(word.as_str());
+            }
+            execute!(
+                stdout(),
+                SetForegroundColor(Color::Black),
+                SetBackgroundColor(Color::Green),
+                Print("<- OK".to_string()),
+                SetForegroundColor(Color::Green),
+                SetBackgroundColor(Color::Reset),
+                Print(str),
+                ResetColor,
+                Print("\n".to_string())
+            ).unwrap();
+        }
+        Err(err) => {
+            execute!(
+                stdout(),
+                SetForegroundColor(Color::Black),
+                SetBackgroundColor(Color::Red),
+            ).unwrap();
+
+            match err {
+                RconError::Other(str) => {
+                    // println!("{}", str.on_dark_red());
+                    execute!(
+                        stdout(),
+                        Print("<- Error".to_string()),
+                        SetForegroundColor(Color::Red),
+                        SetBackgroundColor(Color::Reset),
+                        Print(" ".to_string()),
+                        Print(str)
+                    ).unwrap();
+                },
+                RconError::ConnectionClosed => {
+                    print_error_type("Connection Closed").unwrap();
+                },
+                RconError::InvalidArguments {our_query: _} => {
+                    print_error_type("Invalid Arguments").unwrap();
+                },
+                RconError::UnknownCommand {our_query: _} => {
+                    print_error_type("Unknown Command").unwrap();
+                },
+                _ => panic!("Unexpected error: {:?}", err),
+            };
+            execute!(
+                stdout(),
+                ResetColor,
+                Print("\n".to_string())
+            ).unwrap();
+        }
     }
 
     Ok(())
