457f9db3d0d0b7328c7612bfa70fb0da86d4e3b9
Kiiya <snoewflaek@gmail.com>
2021-04-05 12:30:44 +0200

    fix weapon enforcer

--- src/weaponforcer.rs
+++ src/weaponforcer.rs
@@ -25,18 +25,18 @@ impl WeaponEnforcer {
                     println!("Weapon: {}", weapon);
                     if let Some(killer) = killer {
                         if weapon == Weapon::Mortar || weapon == Weapon::Ucav {
-                            if let Some(n) = offenses.get(&killer) {
-                                if *n >= 3 {
-                                    let _ = bf4.kick(killer.name.clone(), format!("{} is forbidden on this server!", weapon)).await;
-                                } else {
-                                    let _ = bf4.kill(killer.name.clone()).await; // ignore potential fails with let _ = ...
-                                    let _ = bf4.say(format!("{}: Sorry, but {} is not allowed on this server!", killer, weapon), killer.clone()).await;
-                                }
-                            }
                             let _ = bf4.say_lines(vec![
                                 format!("{}: You have been killed by {} by a forbidden weapon.\n\tThey have been punished for their sins.", victim, killer),
                             ], victim).await;
-                            offenses.entry(killer).and_modify(|n| *n += 1);
+
+                            let n = offenses.entry(killer.clone()).or_insert(0);
+
+                            if *n >= 3 {
+                                let _ = dbg!(bf4.kick(killer.name.clone(), format!("{} is forbidden on this server!", weapon)).await);
+                            } else {
+                                let _ = dbg!(bf4.kill(killer.name.clone()).await); // ignore potential fails with let _ = ...
+                                let _ = bf4.say(format!("{}: Sorry, but {} is not allowed on this server!", killer, weapon), killer.clone()).await;
+                            }
                         }
                     }
                 },
