074e337de1bf7b9146d2be8ad16f854b5495d17d
Kiiya <snoewflaek@gmail.com>
2021-04-10 13:57:59 +0200

    Custom serialization for EA GUIDs / Eaid

--- battlefield_rcon/src/bf4/ea_guid.rs
+++ battlefield_rcon/src/bf4/ea_guid.rs
@@ -1,16 +1,63 @@
 use std::{convert::TryInto, fmt::Display, str::FromStr};
 
 use ascii::{AsAsciiStr, AsciiChar, AsciiStr, AsciiString};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::{Error, Visitor}};
 
 #[derive(Debug, Clone)]
 pub struct EaidParseError;
 
 /// EA GUID. Encoded as 32-long hex, without the EA_ prefix.
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Eaid([AsciiChar; 32]);
 
-// pub const EAID_SERVER : Eaid = Eaid([AsciiChar::_0; 32]);
+impl Serialize for Eaid {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer
+    {
+        serializer.serialize_str(self.to_ascii().as_str())
+    }
+}
+
+impl<'de> Deserialize<'de> for Eaid {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de> {
+            deserializer.deserialize_str(EaidVisitor)
+    }
+}
+
+struct EaidVisitor;
+
+impl <'de> Visitor<'de> for EaidVisitor {
+    type Value = Eaid;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.write_str("an EA GUID in form of EA_0123456789ABCDEF...F")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match AsciiStr::from_ascii(v) {
+            Ok(ascii) => {
+                match Eaid::from_rcon_format(ascii) {
+                    Ok(eaid) => Ok(eaid),
+                    Err(_) => Err(E::custom(format!("Not a valid EA GUID: {}", v))),
+                }
+            }
+            Err(_) => Err(E::custom(format!("Not valid ASCII when parsing EA GUID: {}", v))),
+        }
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+            E: Error, {
+        self.visit_str(v.as_str())
+    }
+}
+
 
 impl Eaid {
     /// Input: "EA_FFFF..."
@@ -49,5 +96,25 @@ impl Display for Eaid {
 impl std::fmt::Debug for Eaid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "EA_{}", self.0.as_ascii_str().unwrap())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::bf4::Player;
+
+
+    #[test]
+    fn serialization() {
+        let eaid = Eaid([AsciiChar::F; 32]);
+        let player = Player {
+            name: AsciiString::new(),
+            eaid,
+        };
+        let ser = serde_json::to_string(&player).unwrap();
+        assert_eq!(r#"{"name":"","eaid":"EA_FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"}"#, ser);
+        let de : Player = serde_json::from_str(ser.as_str()).unwrap();
+        assert_eq!(de, player);
     }
 }
