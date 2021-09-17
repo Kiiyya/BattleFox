d04c93c26b324edfba60a1cbe794eb69733ebbfa
Kiiya <snoewflaek@gmail.com>
2021-09-17 22:55:30 +0200

    Basic dockerized rocket+react

--- bfox_web/README.md
+++ bfox_web/README.md
@@ -3,8 +3,15 @@ We use Rocket as a backend (for json API endpoints), and to serve a React SPA.
 The React SPA lives under `./frontend` in this folder.
 
 ## Running to dev stuff
-First run the rocket backend (make sure you config the mongodb db, e.g. via docker).
-Then you run `yarn start` in the `frontend` directory.
+<!-- First run the rocket backend (make sure you config the mongodb db, e.g. via docker).
+Then you run `yarn start` in the `frontend` directory. -->
+Idk man.
+
+But remember to put the following in your `.env` located in `/bfox_web/` (port as specified in
+`/docker-compose-development.yml`):
+```
+MONGO_DB=mongodb://localhost:27017/
+```
 
 ## Packaging to deployment
 That's what a Dockerfile will be for, eventually.
--- bfox_web/src/main.rs
+++ bfox_web/src/main.rs
@@ -1,13 +1,31 @@
+use std::env;
+
+use dotenv::dotenv;
 use rocket_db_pools::Database;
-use rocket::launch;
+use rocket::{figment::map, fs::FileServer, get, launch, response::Redirect, routes};
 
 #[derive(Debug, Database)]
 #[database("bfox_web")]
 pub struct Db(mongodb::Client);
 
+// #[get("/")]
+// async fn index() -> Redirect {
+//     Redirect::temporary("/react-app")
+// }
+
 /// This is `fn main() { ... }` but needlessly spiced up with macros.
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    dotenv().ok();
+
+    let db_url = env::var("MONGO_DB").expect("Please specify the `MONGO_DB` env var.");
+
+    let figment = rocket::Config::figment()
+        .merge(("address", "0.0.0.0"))
+        .merge(("databases.bfox_web", map!["url" => db_url]));
+
+    rocket::custom(figment)
         .attach(Db::init())
+        // .mount("/", routes![index])
+        .mount("/", FileServer::from("frontend/build"))
 }
