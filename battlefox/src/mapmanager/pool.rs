d8826f605ec81f3b144dbd9395be0516038e01b4
Kiiya <snoewflaek@gmail.com>
2022-01-30 21:26:59 +0100

    Use short() to render ballots

--- battlefox/src/mapmanager/pool.rs
+++ battlefox/src/mapmanager/pool.rs
@@ -18,7 +18,7 @@ pub struct MapInPool {
 
 impl Display for MapInPool {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.map.Pretty())
+        write!(f, "{}", self.map.short())
     }
 }
 
